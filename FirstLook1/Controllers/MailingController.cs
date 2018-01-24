42d65ba03b6dee4d176bd808cca0ba5cc9df3a3a
jbrown53 <jbrown53@seattlecentral.edu>
2018-01-24 11:15:09 -0800

    initial commit

---
+++ FirstLook1/Controllers/MailingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstLook1.Models;
+
+namespace FirstLook1.Controllers
+{
+    public class MailingController : Controller
+    {
+        // GET: Mailing
+        public ActionResult Index()
+        {
+            Mailing justin = new Mailing();
+            justin.firstName = "Justin";
+            justin.lastName = "Brown";
+            justin.email = "Justin@email.com";
+
+            Mailing john = new Mailing();
+            john.firstName = "John";
+            john.lastName = "Henry";
+            john.email = "steel.driving@man.com";
+
+            Mailing jeff = new Mailing();
+            jeff.firstName = "Jeff";
+            jeff.lastName = "Ngyuen";
+            jeff.email = "jeff@email.net";
+
+            List<Mailing> MailingList = new List<Mailing>();
+            MailingList.Add(justin);
+            MailingList.Add(john);
+            MailingList.Add(jeff);
+
+            return View(MailingList);
+        }
+    }
+}
---
+++ FirstLook1/Models/Mailing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstLook1.Models
+{
+    public class Mailing
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+    }
+}
